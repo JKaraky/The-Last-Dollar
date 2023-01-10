75672e20ab72e47a2f2dbbae9f6b309472fd4d0c
JKaraky <jawad.alkaraky@gmail.com>
2023-01-10 20:25:10 +0200

    another small edit of last push

--- Assets/Scripts/Projectiles/GunControl.cs
+++ Assets/Scripts/Projectiles/GunControl.cs
@@ -10,6 +10,7 @@ public class GunControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // To rotate gun
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
         Vector3 rotationVector = mousePos - transform.position; // returns a vector pointing towards the mouse position
@@ -17,6 +18,12 @@ public class GunControl : MonoBehaviour
         float rotationZ = Mathf.Atan2(rotationVector.y, rotationVector.x) * Mathf.Rad2Deg; // returns the angle of rotation towards the mouse position in degrees
 
         transform.rotation = Quaternion.Euler(0, 0, rotationZ); // rotates the object accordingly
+
+        // To shoot projectiles
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ProjectileSpawning.projectilePoolInstance.projectilePool.Get();
+        }
     }
 }
 
