85bb34562eae649256b37ec9a49a25b22f79db86
JKaraky <jawad.alkaraky@gmail.com>
2023-01-10 19:58:02 +0200

    Shooting script prototype
    The projectiles are getting shot but some of them get stuck in the starting point.

---
+++ Assets/Scripts/Projectiles/GunPositioning.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPositioning : MonoBehaviour
+{
+    [SerializeField] Camera mainCam;
+    private Vector3 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotationVector = mousePos - transform.position; // returns a vector pointing towards the mouse position
+
+        float rotationZ = Mathf.Atan2(rotationVector.y, rotationVector.x) * Mathf.Rad2Deg; // returns the angle of rotation towards the mouse position in degrees
+
+        transform.rotation = Quaternion.Euler(0, 0, rotationZ); // rotates the object accordingly
+    }
+}
+
--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -6,18 +6,23 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private int projectileSpeed;
     [SerializeField] private Rigidbody2D projectileRb;
+    private Vector3 mousePos;
+    private Camera mainCam;
     private float maxBorder = 14.5f;
 
     // Once the projectile is shot it just keeps going forward
     void Start()
     {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         projectileRb = GetComponent<Rigidbody2D>();
-        projectileRb.velocity = transform.TransformDirection(Vector2.up) * projectileSpeed;
+        Vector3 direction = mousePos - transform.position;
+        projectileRb.velocity = new Vector2(direction.x, direction.y).normalized * projectileSpeed;
     }
 
-    // if the projectile goes out of bounds it is returned to the pool
     void Update()
     {
+        // if the projectile goes out of bounds it is returned to the pool
         if (transform.position.x > maxBorder || transform.position.x < -maxBorder)
         {
             ProjectileSpawning.projectilePoolInstance.projectilePool.Release(this);
