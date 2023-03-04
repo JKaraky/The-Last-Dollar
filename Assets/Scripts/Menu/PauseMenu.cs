ec3898f50907ee2fbd665cf8c83e5398ca44cbfa
MostafaH-01 <mostafa.harb01@gmail.com>
2023-03-04 17:38:07 +0200

    Tweaks for the pause menu I guess?
    Nese sara7a sho kinet 3emil bas 2ilet badda push

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [Header("Levels To Load")]
+    public string _easyLevel;
+    public string _mediumLevel;
+    public string _hardLevel;
+    private string levelToLoad;
 
     public static bool gameIsPaused;
 
@@ -30,6 +36,19 @@ public class PauseMenu : MonoBehaviour
         {
             Time.timeScale = 1;
         }
+    }
+
+    public void NewEasyGameDialog()
+    {
+        SceneManager.LoadScene(_easyLevel);
+    }
+    public void NewMediumGameDialog()
+    {
+        SceneManager.LoadScene(_mediumLevel);
+    }
+    public void NewHardGameDialog()
+    {
+        SceneManager.LoadScene(_hardLevel);
     }
     public void ExitButton()
     {
